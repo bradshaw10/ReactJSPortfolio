75a08c0e62478080163640f459a5faf817fe7b0f
bradshaw10 <eoghan.bradshaw@gmail.com>
2022-03-16 00:18:10 +0400

    Update Hero.js

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -9,12 +9,12 @@ const Hero = (props) => (
     <LeftSection>
       <SectionTitle>
         Welcome To <br />
-        My Website
+        My Portfolio Website
       </SectionTitle>
       <SectionText>
-        Sure let me know what you think Frankie
+        My name is Eoghan Bradshaw. I am an Irish developer based in Dubai. My Experience is as a Software Developer, specialising in Mobile Applications Development with 4+ years’ industry experience at Retail Integration Ltd, one of Ireland’s leading EPOS providers. Experience in mobile development through Xamarin Forms, Unity, Native Android through Java. In addition, I am currently studying to become certified in both Flutter and Kotlin, having created recreational projects in both. Aside from mobile, I have Full-Stack web experience through .Net Core and AngularTS. 
       </SectionText>
-      <Button onClick={() => window.location = "https://google.com"}> Learn More</Button>
+      <Button onClick={() => window.location = "https://docs.google.com/document/d/1wrOXIRyu-5wTEbQWuDxjK_57yANkpxPbyzPYsr_ajV4/edit?usp=sharing"}> View my CV</Button>
     </LeftSection>
   </Section>
 );
